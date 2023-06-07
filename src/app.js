00d322646795013097e46748b7dc2b9599bb9677
Edward-SI03 <tlwlsduq95@gmail.com>
2023-06-08 03:09:50 +0900

    feat: MySQL 연결 초안

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ const Router = require("./routes/index.js");
 const app = express();
 const port = process.env.PORT;
 
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+});
+
+connection.connect();
+
+connection.query("SELECT * from user", (error, rows, fields) => {
+  if (error) throw error;
+  console.log("User info is: ", rows);
+});
+
+connection.end();
+
 const passport = require("passport");
 const passportConfig = require("./passport");
 

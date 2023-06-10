5574572de9bae7bf6da4dc2cd8cd839abce09c68
Edward-SI03 <tlwlsduq95@gmail.com>
2023-06-11 03:47:00 +0900

    feat: dbconfig 값 설정

---
+++ src/config/dbconfig.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+};
+
+module.exports = dbConfig;

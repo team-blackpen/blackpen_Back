482e5b505e276de1016047e84450dc101bfecbeb
Edward-SI03 <tlwlsduq95@gmail.com>
2023-06-13 01:28:15 +0900

    feat: cors 설정

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ const express = require("express");
 const Router = require("./routes/index.js");
 const app = express();
 const port = process.env.PORT;
+const cors = require("cors");
+
+const domains = ["http://localhost:3000", "https://jeonhada-xoqca.run.goorm.site"];
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+
+app.use(cors(corsOptions));
 
 const passport = require("passport");
 const passportConfig = require("./passport");

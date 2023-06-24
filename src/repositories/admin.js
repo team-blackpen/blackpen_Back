f27121248dc56e91ab75bc968c88b4b702929204
Edward-SI03 <tlwlsduq95@gmail.com>
2023-06-25 03:45:03 +0900

    fix: 편지지 생성 트렌젝션 적용

--- src/repositories/admin.js
+++ src/repositories/admin.js
@@ -150,6 +150,64 @@ class AdminRepository {
       throw new ErrorCustom(500, "DB ERROR!");
     }
   };
+
+  insPost = async (postData) => {
+    try {
+      const connection = await pool.getConnection(async (corn) => corn);
+      try {
+        await connection.beginTransaction(); // 트랜잭션 적용 시작
+
+        const insPost = `INSERT INTO tb_post (post_title, post_description, status, reg_dt) VALUES (?, ?, 0, ?);`;
+        const [post] = await connection.query(insPost, [postData.post_title, postData.post_description, postData.regDt]);
+        const postNo = post.insertId;
+
+        const insPostArtRel = `INSERT INTO tb_post_artist_rel (artist_no, post_no) VALUES (?, ?);`;
+        await connection.query(insPostArtRel, [postData.artist_no, postNo]);
+
+        const insPostCateRel = `INSERT INTO tb_post_cate_rel (post_cate_no, post_no, view_seq, status, reg_dt) 
+        VALUES (?, ?, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_cate_rel Pcr WHERE post_cate_no = ?), 0, ?);`;
+        await connection.query(insPostCateRel, [postData.post_cate_no, postNo, postData.post_cate_no, postData.regDt]);
+
+        if (postData.hashtag.length > 0) {
+          for (let i in postData.hashtag) {
+            const insHash = `INSERT INTO tb_post_hashtag (post_no, hashtag_title, status, view_seq, reg_dt) 
+            VALUES (?, ?, 0, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_hashtag Ph WHERE post_no = ?), ?);`;
+            await connection.query(insHash, [postNo, postData.hashtag[i].hashtag_title, postNo, postData.regDt]);
+          }
+        }
+
+        if (postData.post_img.length > 0) {
+          for (let i in postData.hashtag) {
+            const insImg = `INSERT INTO tb_post_img (post_no, img_url, status, view_seq, reg_dt) 
+            VALUES (?, ?, 0, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_img Pi WHERE post_no = ?), ?);`;
+            await connection.query(insImg, [postNo, postData.post_img[i].img_url, postNo, postData.regDt]);
+          }
+        }
+
+        if (postData.post_detail_img.length > 0) {
+          for (let i in postData.hashtag) {
+            const insDetailImg = `INSERT INTO tb_post_detail_img (post_no, img_url, status, view_seq, reg_dt) 
+            VALUES (?, ?, 0, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_detail_img Pdi WHERE post_no = ?), ?);`;
+            await connection.query(insDetailImg, [postNo, postData.post_detail_img[i].img_url, postNo, postData.regDt]);
+          }
+        }
+
+        await connection.commit(); // 커밋
+
+        return;
+      } catch (err) {
+        console.log(err);
+        console.log("Query Error!");
+        await connection.rollback(); // 롤백
+        return err;
+      } finally {
+        connection.release();
+      }
+    } catch (err) {
+      console.log("DB ERROR!");
+      return err;
+    }
+  };
 }
 
 module.exports = AdminRepository;
--- src/services/admin.js
+++ src/services/admin.js
@@ -18,30 +18,33 @@ class AdminService {
 
   creatPost = async (postData) => {
     postData.regDt = dayjs().format("YYYY-MM-DD hh:mm:ss");
+    console.log("🚀 ~ file: admin.js:21 ~ AdminService ~ creatPost= ~ postData:", postData);
 
-    const creatPost = await this.adminRepository.creatPost(postData);
-    const postNo = creatPost.insertId;
+    await this.adminRepository.insPost(postData);
 
-    await this.adminRepository.creatArtistRel(postData, postNo);
-    await this.adminRepository.creatCateRel(postData, postNo);
+    // const creatPost = await this.adminRepository.creatPost(postData);
+    // const postNo = creatPost.insertId;
 
-    if (postData.hashtag.length > 0) {
-      for (let i in postData.hashtag) {
-        await this.adminRepository.creatEtc(postNo, "hashtag", postData.hashtag[i].hashtag_title, postData.regDt);
-      }
-    }
+    // await this.adminRepository.creatArtistRel(postData, postNo);
+    // await this.adminRepository.creatCateRel(postData, postNo);
 
-    if (postData.post_img.length > 0) {
-      for (let i in postData.post_img) {
-        await this.adminRepository.creatEtc(postNo, "postImg", postData.post_img[i].img_url, postData.regDt);
-      }
-    }
+    // if (postData.hashtag.length > 0) {
+    //   for (let i in postData.hashtag) {
+    //     await this.adminRepository.creatEtc(postNo, "hashtag", postData.hashtag[i].hashtag_title, postData.regDt);
+    //   }
+    // }
 
-    if (postData.post_detail_img.length > 0) {
-      for (let i in postData.post_detail_img) {
-        await this.adminRepository.creatEtc(postNo, "postDetailImg", postData.post_detail_img[i].img_url, postData.regDt);
-      }
-    }
+    // if (postData.post_img.length > 0) {
+    //   for (let i in postData.post_img) {
+    //     await this.adminRepository.creatEtc(postNo, "postImg", postData.post_img[i].img_url, postData.regDt);
+    //   }
+    // }
+
+    // if (postData.post_detail_img.length > 0) {
+    //   for (let i in postData.post_detail_img) {
+    //     await this.adminRepository.creatEtc(postNo, "postDetailImg", postData.post_detail_img[i].img_url, postData.regDt);
+    //   }
+    // }
     return;
   };
 }

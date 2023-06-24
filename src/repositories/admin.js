9c181152f6677a4e3aacfe8dea08e21cfd34469a
Edward-SI03 <tlwlsduq95@gmail.com>
2023-06-25 04:44:11 +0900

    fix: 편지지 생성 노출 순서 수정

--- src/repositories/admin.js
+++ src/repositories/admin.js
@@ -164,31 +164,28 @@ class AdminRepository {
         const insPostArtRel = `INSERT INTO tb_post_artist_rel (artist_no, post_no) VALUES (?, ?);`;
         await connection.query(insPostArtRel, [postData.artist_no, postNo]);
 
-        const insPostCateRel = `INSERT INTO tb_post_cate_rel (post_cate_no, post_no, view_seq, status, reg_dt) 
+        const insPostCateRel = `INSERT INTO tb_post_cate_rel (post_cate_no, post_no, view_seq, status, reg_dt)
         VALUES (?, ?, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_cate_rel Pcr WHERE post_cate_no = ?), 0, ?);`;
         await connection.query(insPostCateRel, [postData.post_cate_no, postNo, postData.post_cate_no, postData.regDt]);
 
         if (postData.hashtag.length > 0) {
           for (let i in postData.hashtag) {
-            const insHash = `INSERT INTO tb_post_hashtag (post_no, hashtag_title, status, view_seq, reg_dt) 
-            VALUES (?, ?, 0, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_hashtag Ph WHERE post_no = ?), ?);`;
-            await connection.query(insHash, [postNo, postData.hashtag[i].hashtag_title, postNo, postData.regDt]);
+            const insHash = `INSERT INTO tb_post_hashtag (post_no, hashtag_title, status, view_seq, reg_dt) VALUES (?, ?, 0, ?, ?);`;
+            await connection.query(insHash, [postNo, postData.hashtag[i].hashtag_title, i, postData.regDt]);
           }
         }
 
         if (postData.post_img.length > 0) {
           for (let i in postData.hashtag) {
-            const insImg = `INSERT INTO tb_post_img (post_no, img_url, status, view_seq, reg_dt) 
-            VALUES (?, ?, 0, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_img Pi WHERE post_no = ?), ?);`;
-            await connection.query(insImg, [postNo, postData.post_img[i].img_url, postNo, postData.regDt]);
+            const insImg = `INSERT INTO tb_post_img (post_no, img_url, status, view_seq, reg_dt) VALUES (?, ?, 0, ?, ?);`;
+            await connection.query(insImg, [postNo, postData.post_img[i].img_url, i, postData.regDt]);
           }
         }
 
         if (postData.post_detail_img.length > 0) {
           for (let i in postData.hashtag) {
-            const insDetailImg = `INSERT INTO tb_post_detail_img (post_no, img_url, status, view_seq, reg_dt) 
-            VALUES (?, ?, 0, (SELECT IFNULL(MAX(view_seq) + 1, 0) FROM tb_post_detail_img Pdi WHERE post_no = ?), ?);`;
-            await connection.query(insDetailImg, [postNo, postData.post_detail_img[i].img_url, postNo, postData.regDt]);
+            const insDetailImg = `INSERT INTO tb_post_detail_img (post_no, img_url, status, view_seq, reg_dt) VALUES (?, ?, 0, ?, ?);`;
+            await connection.query(insDetailImg, [postNo, postData.post_detail_img[i].img_url, i, postData.regDt]);
           }
         }
 

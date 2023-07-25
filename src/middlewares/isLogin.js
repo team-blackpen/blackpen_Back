321a119ba4530f9bf5bd3d6fc4ac9d9985e5e60a
Edward-SI03 <tlwlsduq95@gmail.com>
2023-07-26 01:30:48 +0900

    feat: 로그인 유무 파악 미들웨어

--- src/middlewares/isLogin.js
+++ src/middlewares/isLogin.js
@@ -4,7 +4,7 @@ const ErrorCustom = require("../middlewares/errorCustom");
 const UserRepository = require("../repositories/user");
 const userRepository = new UserRepository();
 
-// 단순히 로그인 비로그인을 확인하는 미들웨어(비로그인시 locals에 유저정보 담지 않고 그냥 next로 넘어감)
+// 로그인 유무를 확인(로그인을 필수로 사용하는 api 사용, 비로그인시 에러 발생)
 const isLogin = async (req, res, next) => {
   try {
     const accessToken = req.headers.accesstoken;
@@ -15,7 +15,6 @@ const isLogin = async (req, res, next) => {
 
     if (accessToken) {
       const accessAuthType = accessToken.split(" ")[0];
-
       const accessAuthToken = accessToken.split(" ")[1];
 
       if (accessAuthType !== "Bearer") {
@@ -37,7 +36,6 @@ const isLogin = async (req, res, next) => {
       try {
         if (accessVerified) {
           const userNo = accessVerified.user_no;
-
           const user = await userRepository.isUser(userNo);
 
           if (user == undefined) {
@@ -61,4 +59,51 @@ const isLogin = async (req, res, next) => {
   }
 };
 
-module.exports = { isLogin };
+// 단순히 로그인 비로그인을 확인하는 미들웨어(비로그인시 locals에 유저정보 담지 않고 그냥 next로 넘어감)
+const loginCheck = async (req, res, next) => {
+  try {
+    const accessToken = req.headers.accesstoken;
+
+    if (accessToken) {
+      const accessAuthType = accessToken.split(" ")[0];
+      const accessAuthToken = accessToken.split(" ")[1];
+
+      if (accessAuthType !== "Bearer") {
+        throw new ErrorCustom(401, "토큰 타입이 맞지 않습니다.");
+      }
+
+      let accessVerified = null;
+
+      try {
+        accessVerified = jwt.verify(accessAuthToken, process.env.JWT_KEY);
+      } catch (error) {
+        throw new ErrorCustom(401, "토큰이 유효하지 않습니다.");
+      }
+
+      try {
+        if (accessVerified) {
+          const userNo = accessVerified.user_no;
+          const user = await userRepository.isUser(userNo);
+
+          if (user == undefined) {
+            throw new ErrorCustom(401, "존재하지 않은 유저입니다.");
+          }
+
+          res.locals.user = user;
+
+          next();
+        } else {
+          next();
+        }
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { isLogin, loginCheck };

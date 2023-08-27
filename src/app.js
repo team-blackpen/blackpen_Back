139fac3f2b62e682caa4811c2c6a1cd4c89a39b1
Edward-SI03 <tlwlsduq95@gmail.com>
2023-08-28 03:28:10 +0900

    feat: 라우터 로그 남기기

--- src/app.js
+++ src/app.js
@@ -5,21 +5,21 @@ const app = express();
 const port = process.env.PORT;
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
+const morgan = require("morgan");
+
+app.use(morgan("[:remote-addr - :remote-user] [:date[iso]] :method :url HTTP/:http-version :status :response-time ms"));
 
 const domains = ["http://localhost:3000", "https://jeonhada-xoqca.run.goorm.site"];
-
 const corsOptions = {
   origin: "*",
   credentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 };
-
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
 const passport = require("passport");
 const passportConfig = require("./passport");
-
 passportConfig();
 app.use(passport.initialize());
 // app.use(passport.session());

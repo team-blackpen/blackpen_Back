d3092f250f1d6ef6a1b816d508932d9a316eab1d
Edward-SI03 <tlwlsduq95@gmail.com>
2023-10-14 04:20:08 +0900

    fix: 에러 핸들러 서버 에러로그 남기기

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,11 @@
 const ErrorCustom = require("./errorCustom");
 
 module.exports = (err, req, res, next) => {
-  // console.log(err);
-
   if (err instanceof ErrorCustom) {
     return res.status(err.code).json({ result: 1, errMsg: err.message });
   }
+
+  console.error(`errorHandler -  err: `, err);
 
   return res.status(500).json({ result: 1, errMsg: "서버 에러" });
 };

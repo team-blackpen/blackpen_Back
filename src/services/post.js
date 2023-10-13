8733d43d7829f0b50e85cea0d8a4a5f82eba1ff4
Edward-SI03 <tlwlsduq95@gmail.com>
2023-10-14 02:08:16 +0900

    fix: 편지지 상세조회 에러 처리

--- src/services/post.js
+++ src/services/post.js
@@ -1,20 +1,27 @@
+const ErrorCustom = require("../middlewares/errorCustom");
 const PostRepository = require("../repositories/post");
 
 class PostService {
   postRepository = new PostRepository();
 
   getPostDetail = async (userNo, postNo) => {
-    const post = await this.postRepository.postDetail(userNo, postNo);
-    const postHashtag = await this.postRepository.postEtc(postNo, "hashtag");
-    const postImg = await this.postRepository.postEtc(postNo, "postImg");
-    const postDetailImg = await this.postRepository.postEtc(postNo, "postDetailImg");
-    const postPreviewImg = await this.postRepository.postEtc(postNo, "postPreviewImg");
-    post.hashtag = postHashtag;
-    post.post_img = postImg;
-    post.post_detail_img = postDetailImg;
-    post.post_preview_img = postPreviewImg;
+    try {
+      const post = await this.postRepository.postDetail(userNo, postNo);
+      if (!post) throw new ErrorCustom(400, "존재하지 않는 편지지 입니다");
+      const postHashtag = await this.postRepository.postEtc(postNo, "hashtag");
+      const postImg = await this.postRepository.postEtc(postNo, "postImg");
+      const postDetailImg = await this.postRepository.postEtc(postNo, "postDetailImg");
+      const postPreviewImg = await this.postRepository.postEtc(postNo, "postPreviewImg");
+      post.hashtag = postHashtag;
+      post.post_img = postImg;
+      post.post_detail_img = postDetailImg;
+      post.post_preview_img = postPreviewImg;
 
-    return post;
+      return post;
+    } catch (err) {
+      if (err.code == 400) throw err;
+      throw new ErrorCustom(400, "편지지 상세 조회 실패");
+    }
   };
 }
 
